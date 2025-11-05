f587633ff82c9838106eb29182875e8ed054494e
OmerUcler <42037941+OmerUcler@users.noreply.github.com>
2025-11-05 09:13:17 +0300

    Add files via upload

---
+++ MMA agent scraping script on Arc machines.ps1
@@ -0,0 +1,23 @@
+# Get the list of Azure Arc machines in the subscriptions
+$machines = Get-AzConnectedMachine | Select-Object Name, ResourceGroupName
+ 
+# Loop through each machine and delete the extension if it exists
+foreach ($machine in $machines) {
+  # Check if the extension is installed
+  $extension = Get-AzConnectedMachineExtension -MachineName $machine.Name -Name MicrosoftMonitoringAgent -ResourceGroupName $machine.ResourceGroupName
+  if ($extension) {
+    # Delete the extension
+    Remove-AzConnectedMachineExtension -MachineName $machine.Name -Name MicrosoftMonitoringAgent -ResourceGroupName $machine.ResourceGroupName -Force
+    Write-Host "Deleted MicrosoftMonitoringAgent extension from $($machine.Name)"
+  }
+  else {
+    Write-Host "MicrosoftMonitoringAgent extension not found on $($machine.Name)"
+  }
+}
+
+# Light version of the same script
+$myVM = Get-AzConnectedMachine
+foreach($vm in $myVM)
+{
+	Remove-AzConnectedMachineExtension -MachineName $vm.Name -Name MicrosoftMonitoringAgent -ResourceGroupName $vm.ResourceGroupName -Verbose
+}
